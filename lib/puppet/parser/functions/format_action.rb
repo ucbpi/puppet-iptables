aa3b5fe6c24b009c6d9a73725a2bd4a76ce3c126
Aaron Russo <arusso@berkeley.edu>
2013-06-01 16:59:24 -0700

    adding documentation for function

--- lib/puppet/parser/functions/format_action.rb
+++ lib/puppet/parser/functions/format_action.rb
@@ -1,5 +1,20 @@
 module Puppet::Parser::Functions
   newfunction(:format_action, :type => :rvalue,:doc => <<-EOS
+Given an action, ie. ACCEPT/REJECT or a chain name, returns the partial iptables
+rule to facilitate taking the appropriate action.
+
+Examples:
+
+  # returns "-j ACCEPT'
+  format_action('ACCEPT')
+
+  # returns '-j LOG'
+  format_action('LOG')
+
+  # Parse Error
+  format_action(nil)
+  format_action('')
+  format_action('SOME CHAIN')
   EOS
 ) do |args|
     action = args[0]
