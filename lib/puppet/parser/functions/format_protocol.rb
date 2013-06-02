6e69aff0d3d23188c78a6c65a9b7ebd544634845
Aaron Russo <arusso@berkeley.edu>
2013-06-01 17:38:06 -0700

    removing return of version and raw keys

--- lib/puppet/parser/functions/format_protocol.rb
+++ lib/puppet/parser/functions/format_protocol.rb
@@ -1,5 +1,32 @@
 module Puppet::Parser::Functions
   newfunction(:format_protocol, :type => :rvalue,:doc => <<-EOS
+format_protocol( protocol [, version ])
+
+Given a protocol name and ip protocol version (4 or 6), returns the partial
+iptables rule to faciliate the matching on this protocol
+
+If not specified, ip protocol version defaults to 4.
+
+Valid protocols for each ip version:
+  4: 'tcp', 'udp', 'udplite', 'icmp', 'esp', 'ah', 'sctp', 'all'
+  6: 'tcp', 'udp', 'icmpv6', 'esp', 'all'
+
+Examples:
+
+  # returns '-p tcp'
+  format_protocol('tcp',4)
+  format_protocol('tcp',6)
+
+  # returns '-p icmp'
+  format_protocol('icmp',4)
+
+  # returns '-p icmpv6'
+  format_protocol('icmp',6)
+  format_protocol('icmpv6',6)
+
+  # parse error
+  format_protocol('proto')
+  format_protocol('proto',6)
   EOS
 ) do |args|
     protocols = {
@@ -9,7 +36,7 @@ module Puppet::Parser::Functions
 
     protocol = args[0].dup
     version = '4'
-    version = '6' if args[1] =~ /(ip(v)?)?6/i
+    version = '6' if String(args[1]) =~ /(ip(v)?)?6/i
 
     # we'll be nice and translate icmp to icmpv6 when passed icmp for formatting
     # a ipv6 protocol
@@ -19,12 +46,6 @@ module Puppet::Parser::Functions
     raise Puppet::ParseError, "invalid protocol - #{protocol}" \
       unless protocols[version].include?(protocol)  
 
-    r_h = {
-     'protocol' => "-p #{protocol}",
-     'version' => version,
-     'raw' => args[0],
-    }
-
-    return r_h
+    return "-p #{protocol}"
   end
 end
--- spec/functions/format_protocol_spec.rb
+++ spec/functions/format_protocol_spec.rb
@@ -2,35 +2,31 @@ require 'spec_helper'
 
 describe 'format_protocol' do
   context "=> test illegal protocol type" do
-    input = "tcp6"
-    it { should run.with_params(input).and_raise_error(Puppet::ParseError) }
+    it { should run.with_params('tcp6').and_raise_error(Puppet::ParseError) }
   end
   context "=> test illegal protocol type - v4" do
-    input = "tcp6"
-    it { should run.with_params(input,'4').and_raise_error(Puppet::ParseError) }
+    it {
+      should run.with_params('tcp6','4').and_raise_error(Puppet::ParseError)
+    }
   end
   context "=> test illegal protocol type - v6" do
-    input = "tcp6"
-    it { should run.with_params(input,'6').and_raise_error(Puppet::ParseError) }
+    it { 
+      should run.with_params('tcp6','6').and_raise_error(Puppet::ParseError)
+    }
   end
   context "=> test legal protocol" do
-    input = "tcp"
-    output = { 'protocol' => '-p tcp', 'version' => '4', 'raw' => 'tcp' }
-    it { should run.with_params(input).and_return(output) }
+    it { should run.with_params('tcp').and_return( '-p tcp' ) }
   end
   context "=> test legal protocol - v4" do
-    input = "tcp"
-    output = { 'protocol' => '-p tcp', 'version' => '4', 'raw' => 'tcp' }
-    it { should run.with_params(input,'4').and_return(output) }
+    it { should run.with_params('tcp','4').and_return( '-p tcp' ) }
   end
   context "=> test legal protocol - v6" do
-    input = "tcp"
-    output = { 'protocol' => '-p tcp', 'version' => '6', 'raw' => 'tcp' }
-    it { should run.with_params(input,'6').and_return(output) }
+    it { should run.with_params('tcp','6').and_return( '-p tcp' ) }
   end
   context "=> test icmp to icmpv6 assumption" do
-    input = 'icmp'
-    output = { 'protocol' => '-p icmpv6', 'version' => '6', 'raw' => 'icmp' }
-    it { should run.with_params(input,'6').and_return(output) }
+    it { should run.with_params( 'icmp', '6' ).and_return( '-p icmpv6' ) }
+  end
+  context "=> test numeric protocol version passed" do
+    it { should run.with_params('icmp',6).and_return( '-p icmpv6' ) }
   end
 end
