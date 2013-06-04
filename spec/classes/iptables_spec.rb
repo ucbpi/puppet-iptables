c1826303616c85a1046db04429b3af7ad4b7e7ac
Aaron Russo <arusso@berkeley.edu>
2013-06-03 17:57:47 -0700

    adding test for when no parameters are passed

--- spec/classes/iptables_spec.rb
+++ spec/classes/iptables_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'iptables' do
   let(:facts) { { :concat_basedir => '/var/lib/puppet/concat/' } }
 
-  context "=> custom iptables_file" do
+  context "=> invalid custom iptables_file" do
     let(:params) { {'iptables_file' => 'iptables.test' } }
     it { expect { raise_error(Puppet::Error) } }
   end
@@ -21,5 +21,8 @@ describe 'iptables' do
     }
     it { should contain_concat__setup() }
   end
-end
 
+  context "=> no parameters" do
+    it { should contain_concat__setup() }
+  end
+end
