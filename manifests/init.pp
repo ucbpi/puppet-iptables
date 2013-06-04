38d9fe206abf9752a737afd938aac50132a92a1e
Aaron Russo <arusso@berkeley.edu>
2013-06-03 17:07:10 -0700

    fixed up iptables class

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,69 +15,27 @@
 # Defaults to /etc/sysconfig/ip6tables
 #
 class iptables (
-  $file = undef,
-  $file6 = undef
+  $iptables_file = undef,
+  $ip6tables_file = undef,
+  $version = undef
 ) {
   include concat::setup
 
   ##############################################################################
   # Parameter Validation
   ##############################################################################
-  $file_r = $file ? {
+  $config = $iptables_file ? {
     undef   => '/etc/sysconfig/iptables',
-    default => $file,
+    default => $iptables_file,
   }
 
-  $file6_r = $file ? {
-    undef   => '/etc/sysconfig/iptables',
-    default => $file6,
+  $config6 = $ip6tables_file ? {
+    undef   => '/etc/sysconfig/ip6tables',
+    default => $ip6tables_file,
   }
 
-  validate_absolute_path( $file_r )
-  validate_absolute_path( $file6_r )
-
-  # http://tools.ietf.org/html/rfc5424 - severities
-  # http://tools.ietf.org/html/rfc3164 - priorities
-  #
-  $syslog = {
-    priority   => {
-      emerg    => 0,
-      alert    => 1,
-      critical => 2,
-      error    => 3,
-      warn     => 4,
-      notice   => 5,
-      info     => 6,
-      debug    => 7,
-    },
-
-    facility    => {
-      kern      => 0,
-      user      => 1,
-      mail      => 2,
-      daemon    => 3,
-      auth      => 4,
-      syslog    => 5,
-      lpr       => 6,
-      news      => 7,
-      uucp      => 8,
-      clock     => 9, # not official
-      authpriv  => 10,
-      ftp       => 11,
-      ntp       => 12, # not official
-      log_audit => 13, # not official
-      log_alert => 14, # not official
-      cron      => 15,
-      local0    => 16,
-      local1    => 17,
-      local2    => 18,
-      local3    => 19,
-      local4    => 20,
-      local5    => 21,
-      local6    => 22,
-      local7    => 23,
-    }
-  }
+  validate_absolute_path( $config )
+  validate_absolute_path( $config6 )
 
   # This is used to ensure consistent join separators when generating the order
   # for the concat fragments
---
+++ spec/classes/iptables_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe 'iptables' do
+  let(:facts) { { :concat_basedir => '/var/lib/puppet/concat/' } }
+
+  context "=> custom iptables_file" do
+    let(:params) { {'iptables_file' => 'iptables.test' } }
+    it { expect { raise_error(Puppet::Error) } }
+  end
+
+  context "=> invalid custom ip6tables_file" do
+    let(:params) { { 'ip6tables_file' => 'ip6tables.test' } }
+    it { expect { raise_error(Puppet::Error) } }
+  end
+
+  # if all goes well, we should have a concat::setup object
+  context "=> custom iptables_file and ip6tables_file" do
+    let(:params) {
+      { 'iptables_file' => '/etc/sysconfig/iptables',
+        'ip6tables_file' => '/etc/sysconfig/ip6tables' }
+    }
+    it { should contain_concat__setup() }
+  end
+end
+
