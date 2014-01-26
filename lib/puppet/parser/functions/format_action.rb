dcfbca995512b7963a9863a507678258e5852c4e
Aaron Russo <aaron.n.russo@gmail.com>
2014-01-26 01:14:20 -0800

    issue #19 - fix failed tests for format_action

--- lib/puppet/parser/functions/format_action.rb
+++ lib/puppet/parser/functions/format_action.rb
@@ -7,20 +7,20 @@ Examples:
 
   # returns "-j ACCEPT'
   format_action('ACCEPT')
+  format_action(nil)
 
   # returns '-j LOG'
   format_action('LOG')
 
   # Parse Error
-  format_action(nil)
   format_action('')
   format_action('SOME CHAIN')
   EOS
 ) do |args|
     action = 'ACCEPT'
     action = args[0] unless args[0] == nil
-    
-    if action == nil or action == :undef
+
+    if action == :undef or action == ''
       raise Puppet::ParseError, \
         "action not specified"
     end
@@ -28,7 +28,7 @@ Examples:
     # do some basic validation of the action
     if action =~ /\s/
       raise Puppet::ParseError, \
-        "action cannot contain whitepace - \"#{action}\""
+        "action cannot contain whitespace - \"#{action}\""
     end
 
     return "-j #{action}"
